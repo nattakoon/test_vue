8abdb138dd65b472911cfb646c306d3c304b7aad
bandithimmawan <himmawan444@gmail.com>
2023-11-14 10:17:27 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+import test from './components/test.vue';
+</script>
+
+<template>
+  <header>
+  <TheWelcome/>
+  </header>
+
+  <main>
+   <test/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <body>
+            <h1>สวัสดีครับสุดหล่อ ฮัฟฟู้งงงงงง</h1>
+            <p>ไปไหนดี</p>
+            <p>อยากไปแอ่ว</p>
+            <p>ไปปาก</p>
+        </body>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
